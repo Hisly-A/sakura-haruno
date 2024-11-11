c65faf620d0fb3fe92e1dea9c3af655189336a04
Hisly-A <72717205+Hisly-A@users.noreply.github.com>
2024-11-11 19:55:44 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,9 @@
 CREATE TABLE dados (
-    AlunoID int,
+    PacienteID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
+    HistoricoMedico varchar(200),
     Host varchar(50)
 );
